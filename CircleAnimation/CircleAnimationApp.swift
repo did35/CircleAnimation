13f0937a03cda90841e2c36ad10de65692647f34
Didier <did35@yahoo.com>
2022-03-18 08:54:20 +0100

    Initial Commit

---
+++ CircleAnimation/CircleAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleAnimationApp.swift
+//  CircleAnimation
+//
+//  Created by Didier Delhaisse on 18/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CircleAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
